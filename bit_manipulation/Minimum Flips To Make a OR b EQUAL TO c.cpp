13fbcafd1f97ecfd54309a244de71c8bfe9a5702
Rohan <kamathrohan.9903@gmail.com>
2025-08-15 22:46:10 +0530

    solved Minimum Flips To Make a Or b EQUAL TO c

---
+++ bit_manipulation/Minimum Flips To Make a OR b EQUAL TO c.cpp
@@ -0,0 +1,40 @@
+// Approach 1 
+
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int flips = 0;
+
+        while(a != 0 || b != 0 || c != 0) {
+            if(c & 1) {
+                if((a&1) == 0 && (b&1) == 0) {
+                    flips++;
+                }
+            } else {
+                if(a&1){
+                    flips++;
+                } 
+
+                if(b&1) {
+                    flips++;
+                }
+            }
+
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+
+        return flips;
+    }
+};
+
+// Approach 2
+
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int ans = (a | b) ^ c;
+        return __builtin_popcount(ans) + __builtin_popcount(ans & (a & b));
+    }
+};
