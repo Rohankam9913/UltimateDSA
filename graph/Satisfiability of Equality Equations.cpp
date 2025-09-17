3fea20c3c40f54e28bcb1e1b15db4c26f9d49be6
Rohan <kamathrohan.9903@gmail.com>
2025-09-17 21:02:58 +0530

    solved Satisfiability Of Equations

--- graph/Satisfiability of Equality Equations.cpp
+++ graph/Satisfiability of Equality Equations.cpp
@@ -61,3 +61,111 @@ public:
         return true;
     }
 };
+
+// Using DFS
+
+class Solution {
+public:
+    bool dfs(int u,int dest,unordered_map<int,vector<int>>&adj,vector<bool>&vis) {
+        if(u == dest) {
+            return true;
+        }
+
+        vis[u] = true;
+        for(int &v : adj[u]) {
+            if(!vis[v] && dfs(v,dest,adj,vis)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool equationsPossible(vector<string>& equations) {
+        int n = equations.size();
+
+        unordered_map<int,vector<int>>adj;
+        for(string &equation : equations) {
+            int u = equation[0]-'a';
+            int v = equation[3]-'a';
+
+            if(equation[1] == '=') {
+                adj[u].push_back(v);
+                adj[v].push_back(u);
+            }
+        }
+
+        for(string &equation : equations) {
+            int u = equation[0]-'a';
+            int v = equation[3]-'a';
+
+            if(equation[1] == '!') {
+                vector<bool>vis(26,false);
+                if(dfs(u,v,adj,vis)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+// Using BFS
+
+class Solution {
+public:
+    bool bfs(int src,int dest,unordered_map<int,vector<int>>&adj) {
+        vector<bool>vis(26,false);
+        vis[src] = true;
+
+        queue<int>que;
+        que.push(src);
+
+        while(!que.empty()) {
+            int u = que.front();
+            que.pop();
+
+            if(u == dest) {
+                return true;
+            }
+
+            for(int &v : adj[u]) {
+                if(!vis[v]) {
+                    que.push(v);
+                    vis[v] = true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool equationsPossible(vector<string>& equations) {
+        int n = equations.size();
+
+        unordered_map<int,vector<int>>adj;
+        for(string &equation : equations) {
+            int u = equation[0]-'a';
+            int v = equation[3]-'a';
+
+            if(equation[1] == '=') {
+                adj[u].push_back(v);
+                adj[v].push_back(u);
+            }
+        }
+
+        for(string &equation : equations) {
+            int u = equation[0]-'a';
+            int v = equation[3]-'a';
+
+            if(equation[1] == '!') {
+                if(bfs(u,v,adj)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
